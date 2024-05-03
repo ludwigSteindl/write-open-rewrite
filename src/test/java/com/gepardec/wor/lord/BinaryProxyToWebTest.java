92f64d952d1984de649052ea6a0c3fb1df54e440
sgartner03 <simon.gartner@gepardec.com>
2024-05-03 14:57:16 +0200

    Created Test for binary proxy 2 web

---
+++ src/test/java/com/gepardec/wor/lord/BinaryProxyToWebTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gepardec.wor.lord;
+
+import com.yourorg.AssertEqualsToAssertThat;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class BinaryProxyToWebTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new BinaryProxyToWeb());
+    }
+
+    @DocumentExample
+    @Test
+    public void twoArgument() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              package com.gepardec.wor.lord;
+              
+              public class Test {
+                  public AuMhHostInfoResponseDto getAuMhHostInfo(AuMhHostInfoRequestDto req) {
+                      AuMhHostInfoResponseDto ret = callSvcProxy(req);
+                      
+                      return ret;
+                  }
+                  
+                  
+                  AuMhHostInfoResponseDto callSvcProxy(AuMhHostInfoRequestDto req) {return null;}
+              }
+              
+              public class AuMhHostInfoResponseDto {
+                  public Integer getCallStatus() {
+                      return null;
+                  }
+              }
+              
+              public class AuMhHostInfoRequestDto {}
+              """,
+            """
+              package com.gepardec.wor.lord;
+              
+              public class Test {
+                  public AuMhHostInfoResponseDto getAuMhHostInfo(AuMhHostInfoRequestDto req) {
+                      final boolean useWeb = true;
+                      AuMhHostInfoResponseDto ret = callSvcProxy(req);
+                      
+                      return ret;
+                  }
+                  
+                  
+                  AuMhHostInfoResponseDto callSvcProxy(AuMhHostInfoRequestDto req) {return null;}
+              }
+              
+              public class AuMhHostInfoResponseDto {
+                  public Integer getCallStatus() {
+                      return null;
+                  }
+              }
+              
+              public class AuMhHostInfoRequestDto {}
+              """
+          )
+        );
+    }
+}

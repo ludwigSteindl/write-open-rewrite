87159db349b7b33f90f24dbe6ada31dec4ec6001
Sascha Pöcksteiner <sascha.poecksteiner@gepardec.com>
2024-05-03 12:21:47 +0200

    added ChangeTypeTest in order to figure out how ChangeType works...

---
+++ src/test/java/com/gepardec/wor/ChangeTypeTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gepardec.wor;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.java.ChangeType;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class ChangeTypeTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new ChangeType("java.util.ArrayList", "java.util.List", null))
+          .parser(JavaParser.fromJavaVersion());
+    }
+
+    @Test
+    void changeType() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.util.ArrayList;
+              import java.util.List;
+                            
+              class MyTest {
+                void testFoo() {
+                    ArrayList<Integer> values = new ArrayList<>(List.of(1,2,3));
+                    values.forEach(v->System.out.println(v));
+                }
+              }
+              """,
+            """
+              import java.util.List;
+                            
+              class MyTest {
+                void testFoo() {
+                    List<Integer> values = new List<>(List.of(1,2,3));
+                    values.forEach(v->System.out.println(v));
+                }
+              }
+              """
+          )
+        );
+    }
+}

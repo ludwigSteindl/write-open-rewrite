cbb1c8e66bd752b36446d93bea16eb4606e13906
sgartner03 <simon.gartner@gepardec.com>
2024-05-10 13:28:28 +0200

    LORD Cleaned up ternary visitor

--- src/main/java/com/gepardec/wor/lord/call/ternaries/BinaryProxyToWebConfigClassVisitor.java
+++ src/main/java/com/gepardec/wor/lord/call/ternaries/BinaryProxyToWebConfigClassVisitor.java
@@ -1,5 +1,6 @@
 package com.gepardec.wor.lord.call.ternaries;
 
+import org.jetbrains.annotations.NotNull;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
@@ -7,9 +8,10 @@ import org.openrewrite.java.tree.J;
 
 public class BinaryProxyToWebConfigClassVisitor extends JavaIsoVisitor<ExecutionContext> {
     private static final String CLASS_NAME = "ElgkkPropertiesUtil";
+    private static final String USE_WEB_GETTER_NAME = "isUseWeb";
     private static final JavaTemplate USE_WEB_GETTER = JavaTemplate
             .builder("""
-                    public static boolean isUseWeb() {
+                    public static boolean #{}() {
                         return true;
                     }
                     """)
@@ -17,23 +19,47 @@ public class BinaryProxyToWebConfigClassVisitor extends JavaIsoVisitor<Execution
     @Override
     public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDeclaration, ExecutionContext ctx) {
         classDeclaration = super.visitClassDeclaration(classDeclaration, ctx);
-
-        boolean isNotConfigClass = !classDeclaration.getName().getSimpleName().equals(CLASS_NAME);
-        if (isNotConfigClass) {
+        
+        if (hasName(classDeclaration, CLASS_NAME)) {
             return classDeclaration;
         }
 
+        if (hasMethodWithName(classDeclaration, USE_WEB_GETTER_NAME)) {
+            return classDeclaration;
+        }
+
+        return addUseWebGetter(classDeclaration, USE_WEB_GETTER_NAME);
+    }
+
+    /**
+     * Example: For useWebGetterName = "isUseWeb" add following snippet to class:
+     *     public static boolean isUseWeb() {
+     *         return true;
+     *     }
+     *
+     * @param classDeclaration Class Definition which the getter should be added to
+     * @param useWebGetterName Name of the getter
+     * @return Class Definition with added getter
+     */
+    @NotNull
+    private J.ClassDeclaration addUseWebGetter(J.ClassDeclaration classDeclaration, String useWebGetterName) {
+        return USE_WEB_GETTER.apply(
+                updateCursor(classDeclaration),
+                classDeclaration.getBody().getCoordinates().lastStatement(),
+                useWebGetterName
+        );
+    }
+
+    private static boolean hasName(J.ClassDeclaration classDeclaration, String className) {
+        boolean isNotConfigClass = !classDeclaration.getName().getSimpleName().equals(className);
+        return isNotConfigClass;
+    }
+
+    private static boolean hasMethodWithName(J.ClassDeclaration classDeclaration, String useWebGetterName) {
         boolean hasIsUseWebAlready = classDeclaration.getBody().getStatements().stream()
                 .filter(J.MethodDeclaration.class::isInstance)
                 .map(J.MethodDeclaration.class::cast)
-                .anyMatch(methodDeclaration -> methodDeclaration.getName().getSimpleName().equals("isUseWeb"));
-        if (hasIsUseWebAlready) {
-            return classDeclaration;
-        }
-
-        return USE_WEB_GETTER.apply(
-                updateCursor(classDeclaration),
-                classDeclaration.getBody().getCoordinates().lastStatement()
-        );
+                .anyMatch(methodDeclaration -> methodDeclaration.getName().getSimpleName().equals(useWebGetterName));
+        return hasIsUseWebAlready;
     }
 }
--- src/main/java/com/gepardec/wor/lord/call/ternaries/BinaryProxyToWebTernaryVisitor.java
+++ src/main/java/com/gepardec/wor/lord/call/ternaries/BinaryProxyToWebTernaryVisitor.java
@@ -1,5 +1,6 @@
 package com.gepardec.wor.lord.call.ternaries;
 
+import org.jetbrains.annotations.NotNull;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
@@ -8,33 +9,64 @@ import org.openrewrite.java.tree.J;
 
 public class BinaryProxyToWebTernaryVisitor extends JavaVisitor<ExecutionContext> {
 
+    public static final String BINARY_METHOD_NAME = "callSvcProxy";
+    public static final String WEB_METHOD_NAME = "callSvcWeb";
+
     private static final JavaTemplate WEB_OR_PROXY =
-            JavaTemplate.builder("ElgkkPropertiesUtil.isUseWeb() ? callSvcWeb(#{any()}) : callSvcProxy(#{any()});")
+            JavaTemplate.builder("ElgkkPropertiesUtil.isUseWeb() ? #{}(#{any()}) : #{}(#{any()});")
             .build();
 
     @Override
+    @NotNull
     public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
         method = (J.MethodInvocation) super.visitMethodInvocation(method, ctx);
 
-        if (!method.getSimpleName().equals("callSvcProxy")) {
+        if (!hasName(method, BINARY_METHOD_NAME)) {
             return method;
         }
 
-        J.Ternary surrounding = getCursor().getParentOrThrow().firstEnclosing(J.Ternary.class);
-        if (surrounding != null) {
-            if (surrounding.getTruePart().print(getCursor()).contains("callSvcWeb")) {
-                return method;
-            }
+        if (hasSurroundingTernaryWithWebCall(WEB_METHOD_NAME)) {
+            return method;
         }
 
         Expression argument = method.getArguments().get(0);
         doAfterVisit(new BinaryProxyToWebConfigClassVisitor());
+        return replaceBinaryCallWithTernary(method, WEB_METHOD_NAME);
+    }
+
+    /**
+     * Example: For webMethodName = "callSvcWeb" replace following snippet:
+     *     callSvcProxy(request);
+     * with:
+     *     ElgkkPropertiesUtil.isUseWeb() ? callSvcWeb(request) : callSvcProxy(request);
+     * @param method Method to be replaced
+     * @param webMethodName
+     * @return
+     */
+    private J.Ternary replaceBinaryCallWithTernary(J.MethodInvocation method, String webMethodName) {
+        Expression argument = method.getArguments().get(0);
+        String binaryMethodName = method.getSimpleName();
         return WEB_OR_PROXY.apply(
                 updateCursor(method),
                 method.getCoordinates().replace(),
+
+                webMethodName,
                 argument,
-                argument
-        );
+                binaryMethodName,
+                argument);
     }
+
+    private boolean hasSurroundingTernaryWithWebCall(String webMethodName) {
+        J.Ternary surrounding = getCursor().getParentOrThrow().firstEnclosing(J.Ternary.class);
+        if (surrounding == null) {
+            return false;
+        }
+        return surrounding.getTruePart().print(getCursor()).contains(webMethodName);
+    }
+
+    private static boolean hasName(J.MethodInvocation method, String binaryMethodName) {
+        return method.getSimpleName().equals(binaryMethodName);
+    }
+
 
 }
